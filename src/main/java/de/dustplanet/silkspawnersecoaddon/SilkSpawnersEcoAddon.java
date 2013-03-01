b2ceaefb9f74e2f4a8bb3c783cf291cbbba016b4
xGhOsTkiLLeRx <ratchet.player@gmx.de>
2013-03-01 17:25:30 +0100

    Refractoring: Use Sun coding conventions

--- src/main/java/de/dustplanet/silkspawnersecoaddon/SilkSpawnersEcoAddon.java
+++ src/main/java/de/dustplanet/silkspawnersecoaddon/SilkSpawnersEcoAddon.java
@@ -16,104 +16,115 @@ import net.milkbowl.vault.Vault;
 import net.milkbowl.vault.economy.Economy;
 
 /**
- * General stuff
+ * General stuff (config)
+ * 
  * @author xGhOsTkiLLeRx
  */
 
 public class SilkSpawnersEcoAddon extends JavaPlugin {
-	public FileConfiguration config;
-	private File configFile;
-	public Economy economy;
-	public double defaultPrice = 10.5;
+    public FileConfiguration config;
+    private File configFile;
+    public Economy economy;
+    public double defaultPrice = 10.5;
 
-	public void onEnable() {	
-		// Check for Vault
-		Plugin vault = getServer().getPluginManager().getPlugin("Vault");
-		if (vault != null && vault instanceof Vault) {
-			// If Vault is enabled, load the economy
-			getLogger().info("Loaded Vault successfully");
-			setupEconomy();
-		}
-		else {
-			// Else tell the admin about the missing of Vault
-			getLogger().severe("Vault was not found! Disabling...");
-			setEnabled(false);
-			return;
-		}
-		
-		// Config
-		configFile = new File(getDataFolder(), "config.yml");
-		if(!configFile.exists()){
-			if (configFile.getParentFile().mkdirs()) copy(getResource("config.yml"), configFile);
-		}
-		config = getConfig();
-		loadConfig();
-
-		// Listeners
-		getServer().getPluginManager().registerEvents(new SilkSpawnersEcoAddonListener(this), this);
-
-		// Metrics
-		try {
-			Metrics metrics = new Metrics(this);
-			metrics.start();
-		}
-		catch (IOException e) {
-			getLogger().info("Couldn't start Metrics, please report this!");
-			e.printStackTrace();
-		}
+    public void onEnable() {
+	// Check for Vault
+	Plugin vault = getServer().getPluginManager().getPlugin("Vault");
+	if (vault != null && vault instanceof Vault) {
+	    // If Vault is enabled, load the economy
+	    getLogger().info("Loaded Vault successfully");
+	    setupEconomy();
+	} else {
+	    // Else tell the admin about the missing of Vault
+	    getLogger().severe("Vault was not found! Disabling...");
+	    setEnabled(false);
+	    return;
 	}
 
-	private void loadConfig() {
-		config.options().header("You can configure every entityID/name (without spaces) or a default!");
-		config.addDefault("cantAfford", "&e[SilkSpawnersEco] &4Sorry, but you can't change the mob of this spawner, because you have not enough money!");
-		config.addDefault("afford", "&e[SilkSpawnersEco] &2This action costs &e%money%");
-		config.addDefault("default", 10.5);
-		config.addDefault("pig", 7.25);
-		config.addDefault("cow", 0.00);
-		config.options().copyDefaults(true);
-		saveConfig();
-		defaultPrice = config.getDouble("default");
+	// Config
+	configFile = new File(getDataFolder(), "config.yml");
+	if (!configFile.exists()) {
+	    if (configFile.getParentFile().mkdirs()) {
+		copy(getResource("config.yml"), configFile);
+	    }
+	    else {
+		getLogger().severe("The config folder could NOT be created, make sure it's writable!");
+		getLogger().severe("Disabling now!");
+		setEnabled(false);
+		return;
+	    }
 	}
 
-	// Initialized to work with Vault
-	private Boolean setupEconomy() {
-		RegisteredServiceProvider<Economy> economyProvider = getServer().getServicesManager().getRegistration(net.milkbowl.vault.economy.Economy.class);
-		if (economyProvider != null) {
-			economy = economyProvider.getProvider();
-		}
-		return (economy != null);
+	config = getConfig();
+	loadConfig();
+
+	// Listeners
+	getServer().getPluginManager().registerEvents(new SilkSpawnersEcoAddonListener(this), this);
+
+	// Metrics
+	try {
+	    Metrics metrics = new Metrics(this);
+	    metrics.start();
+	} catch (IOException e) {
+	    getLogger().info("Couldn't start Metrics, please report this!");
+	    e.printStackTrace();
 	}
-	
-	// If no config is found, copy the default one(s)!
-	private void copy(InputStream in, File file) {
-		OutputStream out = null;
-		try {
-			out = new FileOutputStream(file);
-			byte[] buf = new byte[1024];
-			int len;
-			while ((len = in.read(buf)) > 0) {
-				out.write(buf, 0, len);
-			}
-		} catch (IOException e) {
-			getLogger().warning("Failed to copy the default config! (I/O)");
-			e.printStackTrace();
-		} finally {
-			try {
-				if (out != null) {
-					out.close();
-				}
-			} catch (IOException e) {
-				getLogger().warning("Failed to close the streams! (I/O -> Output)");
-				e.printStackTrace();
-			}
-			try {
-				if (in != null) {
-					in.close();
-				}
-			} catch (IOException e) {
-				getLogger().warning("Failed to close the streams! (I/O -> Input)");
-				e.printStackTrace();
-			}
-		}
+    }
+
+    private void loadConfig() {
+	config.options()
+	.header("You can configure every entityID/name (without spaces) or a default!");
+	config.addDefault("cantAfford", "&e[SilkSpawnersEco] &4Sorry, but you can't change the mob of this spawner, because you have not enough money!");
+	config.addDefault("afford", "&e[SilkSpawnersEco] &2This action costs &e%money%");
+	config.addDefault("default", 10.5);
+	config.addDefault("pig", 7.25);
+	config.addDefault("cow", 0.00);
+	config.options().copyDefaults(true);
+	saveConfig();
+	defaultPrice = config.getDouble("default");
+    }
+
+    // Initialized to work with Vault
+    private boolean setupEconomy() {
+	RegisteredServiceProvider<Economy> economyProvider = getServer().getServicesManager().getRegistration(net.milkbowl.vault.economy.Economy.class);
+	if (economyProvider != null) {
+	    economy = economyProvider.getProvider();
 	}
+	return (economy != null);
+    }
+
+    // If no config is found, copy the default one(s)!
+    private void copy(InputStream in, File file) {
+	OutputStream out = null;
+	try {
+	    out = new FileOutputStream(file);
+	    byte[] buf = new byte[1024];
+	    int len;
+	    while ((len = in.read(buf)) > 0) {
+		out.write(buf, 0, len);
+	    }
+	} catch (IOException e) {
+	    getLogger().warning("Failed to copy the default config! (I/O)");
+	    e.printStackTrace();
+	} finally {
+	    try {
+		if (out != null) {
+		    out.close();
+		}
+	    } catch (IOException e) {
+		getLogger().warning(
+			"Failed to close the streams! (I/O -> Output)");
+		e.printStackTrace();
+	    }
+	    try {
+		if (in != null) {
+		    in.close();
+		}
+	    } catch (IOException e) {
+		getLogger().warning(
+			"Failed to close the streams! (I/O -> Input)");
+		e.printStackTrace();
+	    }
+	}
+    }
 }
--- src/main/java/de/dustplanet/silkspawnersecoaddon/SilkSpawnersEcoAddonListener.java
+++ src/main/java/de/dustplanet/silkspawnersecoaddon/SilkSpawnersEcoAddonListener.java
@@ -8,46 +8,49 @@ import de.dustplanet.silkspawners.events.SilkSpawnersSpawnerChangeEvent;
 import de.dustplanet.util.SilkUtil;
 
 public class SilkSpawnersEcoAddonListener implements Listener {
-	private SilkSpawnersEcoAddon plugin;
-	private SilkUtil su;
-	
-	/**
-	 * This is the listener of the custom event
-	 * @author xGhOsTkiLLeRx
-	 */
+    private SilkSpawnersEcoAddon plugin;
+    private SilkUtil su;
 
-	public SilkSpawnersEcoAddonListener(SilkSpawnersEcoAddon instance) {
-		plugin = instance;
-		su = SilkUtil.hookIntoSilkSpanwers();
-	}
+    /**
+     * This is the listener of the custom event to charge the user
+     * 
+     * @author xGhOsTkiLLeRx
+     */
 
-	@EventHandler
-	public void onSpawnerChange(SilkSpawnersSpawnerChangeEvent event) {
-		// Get information
-		Player player = event.getPlayer();
-		short entityID = event.getEntityID();
-		// Get name and replace occuring spaces
-		String name = su.getCreatureName(entityID).toLowerCase().replaceAll(" ", "");
-		double price = plugin.defaultPrice;
-		// Is a specific price listed, yes get it!
-		if (plugin.config.contains(name)) {
-			price = plugin.getConfig().getDouble(name);
-		}
-		// Maybe only the ID is delivered?
-		else if (plugin.config.contains(Short.toString(entityID))) {
-			price = plugin.config.getDouble(Short.toString(entityID));
-		}
-		// If price is 0 or player has free perm, stop here!
-		if (price <= 0 || player.hasPermission("silkspawners.free")) return;
-		// If he has the money, charge it
-		if (plugin.economy.has(player.getName(), price)) {
-			plugin.economy.withdrawPlayer(player.getName(), price);
-			player.sendMessage(ChatColor.translateAlternateColorCodes('&', plugin.config.getString("afford")).replace("%money%", Double.toString(price)));
-		}
-		// Else notify and cancel
-		else {
-			player.sendMessage(ChatColor.translateAlternateColorCodes('&', plugin.config.getString("cantAfford")));
-			event.setCancelled(true);
-		}
+    public SilkSpawnersEcoAddonListener(SilkSpawnersEcoAddon instance) {
+	plugin = instance;
+	su = SilkUtil.hookIntoSilkSpanwers();
+    }
+
+    @EventHandler
+    public void onSpawnerChange(SilkSpawnersSpawnerChangeEvent event) {
+	// Get information
+	Player player = event.getPlayer();
+	short entityID = event.getEntityID();
+	// Get name and replace occuring spaces
+	String name = su.getCreatureName(entityID).toLowerCase().replaceAll(" ", "");
+	double price = plugin.defaultPrice;
+	// Is a specific price listed, yes get it!
+	if (plugin.config.contains(name)) {
+	    price = plugin.getConfig().getDouble(name);
 	}
+	// Maybe only the ID is delivered?
+	else if (plugin.config.contains(Short.toString(entityID))) {
+	    price = plugin.config.getDouble(Short.toString(entityID));
+	}
+	// If price is 0 or player has free perm, stop here!
+	if (price <= 0 || player.hasPermission("silkspawners.free")) {
+	    return;
+	}
+	// If he has the money, charge it
+	if (plugin.economy.has(player.getName(), price)) {
+	    plugin.economy.withdrawPlayer(player.getName(), price);
+	    player.sendMessage(ChatColor.translateAlternateColorCodes('&', plugin.config.getString("afford")).replace("%money%", Double.toString(price)));
+	}
+	// Else notify and cancel
+	else {
+	    player.sendMessage(ChatColor.translateAlternateColorCodes('&', plugin.config.getString("cantAfford")));
+	    event.setCancelled(true);
+	}
+    }
 }
